89345c2e2d658138924fa12353c15e067b6a7fe0
syntaxSizer <kakuma4never@gmail.com>
2016-04-26 00:26:02 +0300

    change video location

--- vertical-fixed-navigation-2/improved/foundation-js/app.js
+++ vertical-fixed-navigation-2/improved/foundation-js/app.js
@@ -1,1 +1,15 @@
+//scroll effect on navigating to other pages
+
 $(document).foundation()
+$('a[href^="#"]').on('click', function (event) {
+
+    var target = $($(this).attr('href'));
+
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
