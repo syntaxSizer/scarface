b6d85db47adac2bb363f91809415a9c4c00aa2d2
syntaxSizer <kakuma4never@gmail.com>
2016-05-21 22:41:01 +0300

    set navigation to fixed

--- vertical-fixed-navigation-2/improved/foundation-js/app.js
+++ vertical-fixed-navigation-2/improved/foundation-js/app.js
@@ -1,19 +1,3 @@
-//scroll effect on navigating to other pages
-
-/**$(document).foundation()
-$('a[href^="#"]').on('click', function (event) {
-
-    var target = $($(this).attr('href'));
-
-    if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-
-});
-*/
 $(function () {
     $('a[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
